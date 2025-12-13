574f11fd322cdd93255399a16275ca1243dd285a
takatan11 <ooame.100915@gmail.com>
2025-12-13 13:05:35 +0900

    @で拡張機能を追加した

---
+++ src/main/java/com/example/reviewer/entity/Cource.java
@@ -0,0 +1,14 @@
+package com.example.reviewer.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name="course")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cource {
+}
