ab8b9585d5d1514046cc7611daa9fc390bb35137
takatan11 <ooame.100915@gmail.com>
2026-01-01 23:08:15 +0900

    SQL文の作成は終わった。あとはHTMLと接続する処理を作成するだけ。

--- src/main/java/com/example/reviewer/repository/CourseRepository.java
+++ src/main/java/com/example/reviewer/repository/CourseRepository.java
@@ -12,12 +12,15 @@ import java.util.List;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
-    @Query("SELECT c FROM Course c")
-
+    @Query("SELECT c FROM Course c WHERE " +
+           "(:faculty IS NULL OR c.faculty = :faculty) AND " +
+           "(:className IS NULL OR c.className LIKE CONCAT('%', :className, '%')) AND " +
+           "(:dayOfClass IS NULL OR c.dayOfClass = :dayOfClass) AND " +
+           "(:teacher IS NULL OR c.teacher LIKE CONCAT('%', :teacher, '%'))")
     List<Course> search(
       @Param("faculty")  Faculty faculty,
       @Param("className") String className,
       @Param("dayOfClass")  DayOfClass dayOfClass,
-      @Param ("teacher")String teacher
+      @Param("teacher") String teacher
     );
 }
